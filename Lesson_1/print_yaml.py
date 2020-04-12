bec5012f43ef29f604e74ffead2fb2c5c95ce5c8
Student User <saranraj002@e.ntu.edu.sg>
2020-04-12 06:37:13 -0700

    initial

---
+++ Lesson_1/print_yaml.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import sys
+import yaml
+from pprint import pprint
+
+
+def read_yaml(filename):
+    with open(filename) as f:
+        return yaml.safe_load(f)
+
+
+if __name__ == "__main__":
+    try:
+        filename = sys.argv[1]
+    except IndexError as e:
+        filename = input("Enter YAML file name: ")
+    pprint(read_yaml(filename))
